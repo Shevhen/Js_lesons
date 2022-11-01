7b98e5f7477f770cfd5db27bfa910563a24284b1
Shevhen <Shevhen9@gmail.com>
2022-11-01 13:01:34 +0100

    Homework Js Lesson 1

---
+++ Leson 1/app.js
@@ -0,0 +1,54 @@
+let hello, owu, com, ua, numInt1, numInt2, numInt3, numInt4, numDouble1, numDouble2, numInt5, bol1, bol2;
+
+hello = 'hello';
+owu = 'owu';
+com = 'com';
+ua = 'ua';
+numInt1 = 1;
+numInt2 = 10;
+numInt3 = -999;
+numInt4 = 123;
+numDouble1 = 3.14;
+numDouble2 = 2.7;
+numInt5 = 16;
+bol1 = true;
+bol2 = false;
+
+console.log(hello, owu, com, ua, numInt1, numInt2, numInt3, numInt4, numDouble1, numDouble2, numInt5, bol1, bol2)
+
+console.log(hello)
+console.log(owu)
+console.log(com)
+console.log(ua)
+console.log(numInt1)
+console.log(numInt2)
+console.log(numInt3)
+console.log(numInt4)
+console.log(numDouble1)
+console.log(numDouble2)
+console.log(numInt5)
+console.log(bol1)
+console.log(bol2)
+
+let firstName, middleName, lastName, person;
+
+firstName = 'Yevhen ';
+middleName = 'Vitalijovych ';
+lastName = 'Shevchenko ';
+person = lastName + firstName + middleName;
+
+console.log(person)
+
+let a = 100;
+let b = '100';
+let c = true;
+
+console.log(typeof a)
+console.log(typeof b)
+console.log(typeof c)
+
+firstNameUser = prompt("Введіть будь-ласка ваше Ім'я (Латинськими літерами)")
+middleNameUser = prompt("Введіть будь-ласка ваше По-Батькові (Латинськими літерами)")
+ageUser = prompt("Скільки вам років?")
+
+console.log(firstNameUser,middleNameUser,ageUser)
